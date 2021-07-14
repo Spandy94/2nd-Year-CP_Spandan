1a18718fb1d073fe29d1967eadd6007b1c6c8136
Spandy94 <pupul.spandan94@gmail.com>
2021-07-14 10:55:59 +0530

    factors

--- 07-isfactor-Python/isfactor.py
+++ 07-isfactor-Python/isfactor.py
@@ -3,7 +3,10 @@
 # if f is a factor of n, and False otherwise. 
 # Note that every integer is a factor of 0.
 
-
-
 def fun_isfactor(f, n):
-	return False # replace with your solution
+
+	if(n%f == 0) and (f != 0):
+		return True # replace with your solution
+
+	else:
+		return False
