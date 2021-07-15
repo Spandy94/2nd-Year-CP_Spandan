80bc619e5ac45f725437cf19b528d53b570785a1
Spandy94 <pupul.spandan94@gmail.com>
2021-07-15 15:30:29 +0530

    area triangle

--- 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
+++ 17-triangleareabycoordinates-Python/triangleareabycoordinates.py
@@ -6,4 +6,22 @@
 
 def triangleareabycoordinates(x1, y1, x2, y2, x3, y3):
 	# your code goes here
-	pass
+	# a = fun_distance(x1,y1,x2,y2)
+	# b = fun_distance(x2,y2,x3,y3)
+	# c = fun_distance(x1,y1,x3,y3)
+
+	# s = (a+b+c)//2
+
+	# Area = (s(s-a)(s-b)(s-c))**(1/2)
+	# return int(Area)
+	Area = abs((0.5)*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2)))
+	return Area
+	
+
+
+# def fun_distance(x1, y1, x2, y2):
+# 	# your code goes here
+# 	leng = ((x2-x1)**2 + (y2-y1)**2)
+# 	dist = (leng)**(1/2)
+	
+# 	return int(dist)
