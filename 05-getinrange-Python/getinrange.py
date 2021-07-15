9628462211810263e9e58fc9a79579d18ef45770
Spandy94 <pupul.spandan94@gmail.com>
2021-07-15 10:24:59 +0530

    range

--- 05-getinrange-Python/getinrange.py
+++ 05-getinrange-Python/getinrange.py
@@ -6,4 +6,11 @@
 
 def fun_getinrange(x, bound1, bound2):
 	# your code goes here
-	return -1
+	if(bound2<x<bound1) or (bound1<x<bound2):
+		return x
+
+	elif(x<bound2):
+		return bound2
+
+	elif(x>bound1):
+		return bound1
