abfc500dc9711aa5dd200e4141f2c7b19df09666
Spandy94 <pupul.spandan94@gmail.com>
2021-07-20 09:05:47 +0530

    intersection2

--- 18-lineintersection-Python/lineintersection.py
+++ 18-lineintersection-Python/lineintersection.py
@@ -8,8 +8,8 @@ def lineintersection(m1, b1, m2, b2):
 	# your code goes here
 	if ((m1 == m2) and (b1 != b2)):
 		return None
-	# elif((m1 == m2) and (b1 == b2)):
-	# 	return None
+	elif((m1 != m2) and (b1 != b2)):
+		return None
 	else:
 		return (b2-b1)/(m1-m2)
 
