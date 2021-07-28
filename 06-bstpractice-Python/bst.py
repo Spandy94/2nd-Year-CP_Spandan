35fdc3ddbce6442dc23d58ef511b06dd1c20401f
Spandy94 <pupul.spandan94@gmail.com>
2021-07-28 15:07:47 +0530

    bst

--- 06-bstpractice-Python/bst.py
+++ 06-bstpractice-Python/bst.py
@@ -10,38 +10,14 @@ class BST(object):
 
     def insert(self, new_val):
         # Your code goes here
-      if self.data:
-         if data < self.data:
-            if self.left is None:
-               self.left = Node(data)
-            else:
-               self.left.insert(data)
-            else data > self.data:
-               if self.right is None:
-                  self.right = Node(data)
-               else:
-                  self.right.insert(data)
-         else:
-            self.data = data
+        pass
 
     def printSelf(self):
         # Your code goes here
-      if self.left:
-         self.left.printSelf()
-      return self.value
-      if self.right:
-         self.right.printSelf()
+        pass
         
     def search(self, find_val):
         # Your code goes here
-      if lkpval < self.data:
-         if self.left is None:
-            return str(lkpval)+" Not Found"
-         return self.left.findval(lkpval)
-       else if lkpval > self.data:
-            if self.right is None:
-               return str(lkpval)+" Not Found"
-            return self.right.findval(lkpval)
-        else:
-            print(str(self.data) + ' is found')
+        pass
+
 
