edd156765e4576cd9a325ac360280c158eacc5f1
Spandy94 <pupul.spandan94@gmail.com>
2021-08-06 12:57:08 +0530

    powset

--- set.py
+++ set.py
@@ -9,4 +9,20 @@
 
 def limitedPowerSet(n, k):
     # Your code goes here...
-    pass
+    powset = [{}]
+    for i in range(1,n+1):
+        z = {i}
+        powset.append(z)
+    for i in range(1,n+1):
+        for j in range(1,n+1):
+            if len(powset) == k:
+                return powset
+            else:
+                if j != i:
+                    y = {i, j}
+                    powset.append(y)
+
+assert(limitedPowerSet(5,7)==[{}, {1}, {2}, {3}, {4}, {5}, {1, 2}])
+assert(limitedPowerSet(6,7)==[{}, {1}, {2}, {3}, {4}, {5}, {6}])
+assert(limitedPowerSet(6,10)==[{}, {1}, {2}, {3}, {4}, {5}, {6}, {1, 2}, {1, 3}, {1, 4}])
+print('Test cases passed')
